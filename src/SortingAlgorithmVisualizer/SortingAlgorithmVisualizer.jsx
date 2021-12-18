0ec3da3ca5e9cc1d34d7fb025b64676040699dbe
calbert3991 <calbert3991@gmail.com>
2021-12-18 13:28:28 -0500

    Updated commit from laptop work at csc on 12-17-21

--- src/SortingAlgorithmVisualizer/SortingAlgorithmVisualizer.jsx
+++ src/SortingAlgorithmVisualizer/SortingAlgorithmVisualizer.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import {getMergeSortAnimations} from '../SortingAlgorithms/SortingAlgorithms.js';
 import './SortingAlgorithmVisualizer.css';
 
+import Button from '../GeneralComponents/Button.js';
+
 // Change this value for the speed of the animations.
 const ANIMATION_SPEED_MS = 1;
 
 // Change this value for the number of bars (value) in the array.
 var NUMBER_OF_ARRAY_BARS = 250;
+
+var MAX_BAR_HEIGHT = 730;
+
+var currentSortingAlgorithm = "mergeSort";
+var currentSortingAlgorithmName = "Select A Sorting Algorithm";
 
 // This is the main color of the array bars.
 const PRIMARY_COLOR = '#09ffd5';
@@ -31,13 +38,15 @@ export default class SortingVisualizer extends React.Component {
   resetArray() {
     const array = [];
     for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
-      array.push(randomIntFromInterval(5, 730));
+      array.push(randomIntFromInterval(5, MAX_BAR_HEIGHT));
     }
     this.setState({array});
   }
 
   mergeSort() {
+    const {array} = this.state;
     const animations = getMergeSortAnimations(this.state.array);
+    var scaleFactor = (MAX_BAR_HEIGHT / Math.max(...array));
     for (let i = 0; i < animations.length; i++) {
       const arrayBars = document.getElementsByClassName('array-bar');
       const isColorChange = i % 3 !== 2;
@@ -54,22 +63,22 @@ export default class SortingVisualizer extends React.Component {
         setTimeout(() => {
           const [barOneIdx, newHeight] = animations[i];
           const barOneStyle = arrayBars[barOneIdx].style;
-          barOneStyle.height = `${newHeight}px`;
+          barOneStyle.height = `calc(${newHeight}px * ${scaleFactor})`;
         }, i * ANIMATION_SPEED_MS);
       }
     }
   }
 
   quickSort() {
-    // We leave it as an exercise to the viewer of this code to implement this method.
+    // Need to implement this method.
   }
 
   heapSort() {
-    // We leave it as an exercise to the viewer of this code to implement this method.
+    // Need to implement this method.
   }
 
   bubbleSort() {
-    // We leave it as an exercise to the viewer of this code to implement this method.
+    // Need to implement this method.
   }
 
   // NOTE: This method will only work if your sorting algorithms actually return
@@ -88,31 +97,69 @@ export default class SortingVisualizer extends React.Component {
     }
   }
 
+  setCurrentSortingAlgorithm(SortingAlgorithm, SortingAlgorithmName){
+    currentSortingAlgorithm = SortingAlgorithm;
+    currentSortingAlgorithmName = SortingAlgorithmName;
+    console.log(currentSortingAlgorithmName);
+
+    this.forceUpdate();
+  }
+
   render() {
     const {array} = this.state;
+    var scaleFactor = (MAX_BAR_HEIGHT / Math.max(...array));
+    console.log(scaleFactor * Math.max(...array));
 
     return (
       <div className="page-container">
         <div className="page-header">Sorting Algorithm Visualizer</div>
         <div className="array-container">
-            {array.map((value, idx) => (
             <div
-                className="array-bar"
-                key={idx}
+                className="array-bar-spacer"
+                key={NUMBER_OF_ARRAY_BARS+1}
                 style={{
-                backgroundColor: PRIMARY_COLOR,
-                width: `calc( (90vw - (4px * ${NUMBER_OF_ARRAY_BARS})) / ${NUMBER_OF_ARRAY_BARS})`,
-                height: `${value}px`,
+                width: `1px`,
+                height: `${MAX_BAR_HEIGHT}px`,
                 }}>
             </div>
+            {array.map((value, idx) => (
+              <div
+                  className="array-bar"
+                  key={idx}
+                  style={{
+                  backgroundColor: PRIMARY_COLOR,
+                  width: `calc( (90vw - (4px * ${NUMBER_OF_ARRAY_BARS}) - 2px) / ${NUMBER_OF_ARRAY_BARS + 2})`,
+                  height: `calc(${value}px * ${scaleFactor})`,
+                  bottom: `0px`,
+                  }}>
+              </div>
             ))}
+            <div
+                className="array-bar-spacer"
+                key={0}
+                style={{
+                width: `1px`,
+                height: `${MAX_BAR_HEIGHT}px`,
+                }}>
+            </div>
         </div>
         <div className='button-container'>
-        <button onClick={() => this.resetArray()}>Generate New Array</button>
-            <button onClick={() => this.mergeSort()}>Merge Sort</button>
-            <button onClick={() => this.quickSort()}>Quick Sort</button>
-            <button onClick={() => this.heapSort()}>Heap Sort</button>
-            <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+          <Button buttonStyle="btn--outline" onClick={() => this.resetArray()}>Generate New Array</Button>
+          <Button buttonStyle="btn--outline" onClick={() => setDropDown("dropdown-1")}>
+            Current Sorting Algorithm: {currentSortingAlgorithmName}
+            <div className="dropdown-1-dropdown-item-container">
+              <Button buttonStyle="btn--outline" onClick={() => this.setCurrentSortingAlgorithm("mergeSort", "Merge Sort")}>Merge Sort</Button>
+              <Button buttonStyle="btn--outline" onClick={() => this.setCurrentSortingAlgorithm("quickSort", "Quick Sort")}>Quick Sort</Button>
+              <Button buttonStyle="btn--outline" onClick={() => this.resetArray()}>Quick Sort</Button>
+              <Button buttonStyle="btn--outline" onClick={() => this.resetArray()}>Quick Sort</Button>
+              <Button buttonStyle="btn--outline" onClick={() => this.resetArray()}>Quick Sort</Button>
+              <Button buttonStyle="btn--outline" onClick={() => this.resetArray()}>Quick Sort</Button>
+              <Button buttonStyle="btn--outline" onClick={() => this.resetArray()}>Quick Sort</Button>
+              <Button buttonStyle="btn--outline" onClick={() => this.resetArray()}>Quick Sort</Button>
+              <Button buttonStyle="btn--outline" onClick={() => this.resetArray()}>Quick Sort</Button>
+            </div>
+          </Button>
+          <Button buttonStyle="btn--outline" onClick={() => this.mergeSort()}>Sort</Button>
         </div>
       </div>
     );
@@ -134,6 +181,15 @@ function arraysAreEqual(arrayOne, arrayTwo) {
     }
   }
   return true;
+}
+
+//Dependency for DropDown Button
+function setDropDown(itemName){
+  var dropdownItems = document.getElementsByClassName(itemName + "-dropdown-item-container");
+  for (let i = 0; i < dropdownItems.length; i++) {
+    var dropdownShow = dropdownItems[i].style.display === "none" ? "block" : "none";
+    dropdownItems[i].style.display  = dropdownShow;
+  }
 }
 
 window.addEventListener('resize', function(event) {
